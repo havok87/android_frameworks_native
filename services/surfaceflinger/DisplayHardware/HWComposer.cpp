0b83acab653c7fa45bb1ee455f8f83d0c5585219
havok87 <andre.sookram@gmail.com>
2014-01-08 13:25:13 -0500

    derp fixes
    Change-Id: I9c736dd783625857a7773dc9e490a48a00fb8f77

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -494,7 +494,7 @@ void HWComposer::eventControl(int disp, int event, int enabled) {
                 const int32_t oldValue = mDisplayData[disp].events & eventBit;
                 if (newValue != oldValue) {
                     ATRACE_CALL();
-                    err = mHwc->eventControl(mHwc, disp, event, enabled);
+                    err = mHwc->eventControl ? mHwc->eventControl(mHwc, disp, event, enabled) : NO_ERROR;
                     if (!err) {
                         int32_t& events(mDisplayData[disp].events);
                         events = (events & ~eventBit) | newValue;
@@ -504,21 +504,6 @@ void HWComposer::eventControl(int disp, int event, int enabled) {
                 // do if it does.
                 ALOGE_IF(err, "eventControl(%d, %d) failed %s",
                          event, enabled, strerror(-err));
-    if (mHwc && !mDebugForceFakeVSync) {
-        // NOTE: we use our own internal lock here because we have to call
-        // into the HWC with the lock held, and we want to make sure
-        // that even if HWC blocks (which it shouldn't), it won't
-        // affect other threads.
-        Mutex::Autolock _l(mEventControlLock);
-        const int32_t eventBit = 1UL << event;
-        const int32_t newValue = enabled ? eventBit : 0;
-        const int32_t oldValue = mDisplayData[disp].events & eventBit;
-        if (newValue != oldValue) {
-            ATRACE_CALL();
-            err = mHwc->eventControl ? mHwc->eventControl(mHwc, disp, event, enabled) : NO_ERROR;
-            if (!err) {
-                int32_t& events(mDisplayData[disp].events);
-                events = (events & ~eventBit) | newValue;
             }
 
             if (err == NO_ERROR && mVSyncThread != NULL) {
@@ -767,8 +752,8 @@ status_t HWComposer::release(int disp) {
     LOG_FATAL_IF(disp >= VIRTUAL_DISPLAY_ID_BASE);
     if (mHwc) {
         eventControl(disp, HWC_EVENT_VSYNC, 0);
-	if (mHwc->blank)
-            return (status_t)mHwc->blank(mHwc, disp, 1);
+        if (mHwc->blank)
+		return (status_t)mHwc->blank(mHwc, disp, 1);
     }
     return NO_ERROR;
 }
@@ -1212,3 +1197,4 @@ HWComposer::DisplayData::~DisplayData() {
 
 // ---------------------------------------------------------------------------
 }; // namespace android
+
