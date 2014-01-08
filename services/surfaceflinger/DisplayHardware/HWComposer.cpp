5fdbc8c9cb5c5c56c5c2c9e5d010ffa4a126a625
havok87 <andre.sookram@gmail.com>
2014-01-08 12:39:33 -0500

    surfaceflinger: Don't crash on missing functions in hwcomposers
    Some hwcomposers -- most notably hwcomposer.default.so -- don't
    implement blank() or eventControl().
    Don't crash when trying to use them.
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    
    Conflicts:
    	services/surfaceflinger/DisplayHardware/HWComposer.cpp
    
    Change-Id: I885bd7e60cf8ea29cf8e01c8eb800c71dbe888ee

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -504,6 +504,21 @@ void HWComposer::eventControl(int disp, int event, int enabled) {
                 // do if it does.
                 ALOGE_IF(err, "eventControl(%d, %d) failed %s",
                          event, enabled, strerror(-err));
+    if (mHwc && !mDebugForceFakeVSync) {
+        // NOTE: we use our own internal lock here because we have to call
+        // into the HWC with the lock held, and we want to make sure
+        // that even if HWC blocks (which it shouldn't), it won't
+        // affect other threads.
+        Mutex::Autolock _l(mEventControlLock);
+        const int32_t eventBit = 1UL << event;
+        const int32_t newValue = enabled ? eventBit : 0;
+        const int32_t oldValue = mDisplayData[disp].events & eventBit;
+        if (newValue != oldValue) {
+            ATRACE_CALL();
+            err = mHwc->eventControl ? mHwc->eventControl(mHwc, disp, event, enabled) : NO_ERROR;
+            if (!err) {
+                int32_t& events(mDisplayData[disp].events);
+                events = (events & ~eventBit) | newValue;
             }
 
             if (err == NO_ERROR && mVSyncThread != NULL) {
@@ -752,14 +767,15 @@ status_t HWComposer::release(int disp) {
     LOG_FATAL_IF(disp >= VIRTUAL_DISPLAY_ID_BASE);
     if (mHwc) {
         eventControl(disp, HWC_EVENT_VSYNC, 0);
-        return (status_t)mHwc->blank(mHwc, disp, 1);
+	if (mHwc->blank)
+            return (status_t)mHwc->blank(mHwc, disp, 1);
     }
     return NO_ERROR;
 }
 
 status_t HWComposer::acquire(int disp) {
     LOG_FATAL_IF(disp >= VIRTUAL_DISPLAY_ID_BASE);
-    if (mHwc) {
+    if (mHwc && mHwc->blank) {
         return (status_t)mHwc->blank(mHwc, disp, 0);
     }
     return NO_ERROR;
